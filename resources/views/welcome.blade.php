8d9ed4b4bb00f72db629885073ce28626fd2306f
Rui2917 <a115861@aeg1.pt>
2025-03-17 16:59:58 +0000

    filtros

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,13 @@
 <x-guestLayout>
     <div class="lg:flex">
         <div class="bg-red-400 lg:w-1/3">
-            <p class="border p-2 rounded m-1">Sopa</p>
-            <p class="border p-2 rounded m-1">Laranja</p>
+            <p class="border p-2 rounded m-1">Cores</p>
+            <p class="border p-2 rounded m-1">Ano</p>
+            <p class="border p-2 rounded m-1">Preço</p>
+            <p class="border p-2 rounded m-1">Ram</p>
+            <p class="border p-2 rounded m-1">Processador</p>
+            <p class="border p-2 rounded m-1">Memória interna</p>
+            <p class="border p-2 rounded m-1">Telemóveis com NFC</p>
         </div>
         <div class="bg-green-400 lg:w-2/3">
 
