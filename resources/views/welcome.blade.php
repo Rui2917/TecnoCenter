e414b462f34f1203d8e5bc45e17375eb087de1e8
David Freitas <dvdfreitas@gmail.com>
2025-03-17 15:57:04 +0000

    250317

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,11 @@
 <x-guestLayout>
-    
+    <div class="lg:flex">
+        <div class="bg-red-400 lg:w-1/3">
+            <p class="border p-2 rounded m-1">Sopa</p>
+            <p class="border p-2 rounded m-1">Laranja</p>
+        </div>
+        <div class="bg-green-400 lg:w-2/3">
+
+        </div>
+    </div>    
 </x-guestLayout>
